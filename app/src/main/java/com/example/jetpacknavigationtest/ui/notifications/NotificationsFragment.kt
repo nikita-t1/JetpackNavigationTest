981c9fc119815d5b360dc9bc3daa3a8e9aed80cc
nikita-t1 <nikita.t2@gmx.de>
2019-12-28 19:01:01 +0100

    Added Gradle

--- app/src/main/java/com/example/jetpacknavigationtest/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/jetpacknavigationtest/ui/notifications/NotificationsFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -23,6 +24,10 @@ class NotificationsFragment : Fragment() {
             ViewModelProviders.of(this).get(NotificationsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_notifications, container, false)
         val textView: TextView = root.findViewById(R.id.text_notifications)
+        val btn: Button = root.findViewById(R.id.btn)
+        btn.setOnClickListener{
+
+        }
         notificationsViewModel.text.observe(this, Observer {
             textView.text = it
         })
